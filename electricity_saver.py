44bf5a52b3a83a968c7f05251a488cb50c559bdd
Shourya512607 <shouryaagarwal2014@gmail.com>
2025-08-23 17:15:55 +0530

    Update electricity_saver.py

--- electricity_saver.py
+++ electricity_saver.py
@@ -4,28 +4,34 @@ import tensorflow as tf
 import RPi.GPIO as GPIO
 import time
 
-# GPIO setup
-RELAY_PIN = 17  # You can change this GPIO pin
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RELAY_PIN, GPIO.OUT)
-GPIO.output(RELAY_PIN, GPIO.LOW)
+# ---------------- Relay Setup ----------------
+RELAYS = {
+    "bulb1": 17,
+    "bulb2": 27,
+    "fan1": 22,
+    "fan2": 23
+}
 
-# Load TFLite model
+GPIO.setmode(GPIO.BCM)
+for pin in RELAYS.values():
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)  # OFF at start
+
+def switch_all(status):
+    for device, pin in RELAYS.items():
+        GPIO.output(pin, GPIO.HIGH if status else GPIO.LOW)
+    print(f"[APPLIANCES] {'ON' if status else 'OFF'}")
+
+# ---------------- Load TFLite Model ----------------
 interpreter = tf.lite.Interpreter(model_path="tiny_person_model.tflite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
+# ---------------- Camera ----------------
 cap = cv2.VideoCapture(0)
-
-def switch_light(status):
-    if status:
-        GPIO.output(RELAY_PIN, GPIO.HIGH)
-        print("[LIGHT] ON")
-    else:
-        GPIO.output(RELAY_PIN, GPIO.LOW)
-        print("[LIGHT] OFF")
+appliances_on = False
 
 try:
     while True:
@@ -34,25 +40,31 @@ try:
             break
 
         # Preprocess for CNN
-        img = cv2.resize(frame, (32,32))
+        img = cv2.resize(frame, (32, 32))
         img = img.astype("float32") / 255.0
         img = np.expand_dims(img, axis=0)
 
-        # Run inference
+        # Run TFLite inference
         interpreter.set_tensor(input_details[0]['index'], img)
         interpreter.invoke()
         pred = interpreter.get_tensor(output_details[0]['index'])
 
         person_detected = pred[0][0] > 0.5
-        switch_light(person_detected)
 
-        cv2.imshow("Electricity Saver (Raspberry Pi)", frame)
+        if person_detected and not appliances_on:
+            switch_all(True)
+            appliances_on = True
+        elif not person_detected and appliances_on:
+            switch_all(False)
+            appliances_on = False
+
+        # Show camera feed
+        cv2.imshow("Electricity Saver", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 except KeyboardInterrupt:
-    print("Exiting...")
-
+    pass
 finally:
     cap.release()
     cv2.destroyAllWindows()
